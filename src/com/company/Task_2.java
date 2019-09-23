25798bdc8291c58832c7947f28ef2aca3a412658
Serhio-Last <serhio.last@gmail.com>
2019-09-23 03:54:12 +0300

    Lessons 1 and 2

--- src/com/company/Task_2.java
+++ src/com/company/Task_2.java
@@ -3,7 +3,7 @@ package com.company;
 public class Task_2 {
     public static void main(String[] args) {
         boolean o = true;
-        byte b = 127;
+        byte b = 126;
         char c = '#';
         short s = -32768;
         int a = 30;
